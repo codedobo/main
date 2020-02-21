bc8e5c5993adb674f673823bd2610b06486d7cf0
CodeDoctorDE <wusel905@gmail.com>
2020-02-21 14:23:19 +0100

    update setup

--- setup.rb
+++ setup.rb
@@ -4,6 +4,11 @@ require_relative './index.rb'
 class MainModule
   def setup
     send_message "\u001b[96mSet up main module..."
+    @client.query("CREATE TABLE IF NOT EXISTS `main` (
+      `SERVERID` int(50) unsigned NOT NULL,
+      `LANGUAGE` varchar(255) NOT NULL,
+      PRIMARY KEY  (`SERVERID`)
+    );")
     send_message "\u001b[32mSuccessfully set up main module!"
   end
 end
