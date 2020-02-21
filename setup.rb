2ca0c2652c35ce0d4837e2d0fe16b9029d33b827
CodeDoctorDE <wusel905@gmail.com>
2020-02-21 14:24:51 +0100

    fix bug

--- setup.rb
+++ setup.rb
@@ -4,7 +4,7 @@ require_relative './index.rb'
 class MainModule
   def setup
     send_message "\u001b[96mSet up main module..."
-    @client.query("CREATE TABLE IF NOT EXISTS `main` (
+    @module_manager.client.query("CREATE TABLE IF NOT EXISTS `main` (
       `SERVERID` int(50) unsigned NOT NULL,
       `LANGUAGE` varchar(255) NOT NULL,
       PRIMARY KEY  (`SERVERID`)
